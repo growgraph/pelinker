e78dd84c699a5cf2ea527642ce4265ecb2d9af4e
Jamshid Sourati <jsourati@uchicago.edu>
2025-06-02 15:16:08 -0500

    Adding a function for extracting and embedding mentions; fixing a bug when a property is not mentioned at all

--- run/mag_run.py
+++ run/mag_run.py
@@ -1,7 +1,9 @@
 import spacy
 import torch
+import numpy as np
 import pandas as pd
 import tqdm
+import pdb
 
 from pelinker.matching import match_pattern
 from pelinker.util import texts_to_vrep
@@ -18,76 +20,89 @@ tokenizer = AutoTokenizer.from_pretrained("NeuML/pubmedbert-base-embeddings")
 
 nlp = spacy.load("en_core_web_sm")
 
-df = pd.read_csv("data/test/sample.tsv", sep="\t", header=None)
+data_path = "data/jamshid/bio_mag_2M.tsv"
+df = pd.read_csv(data_path, 
+                 sep="\t", 
+                 header=None, 
+                 chunksize=2000
+        )
 
-props = ["activates", "enhances", "upregulates", "suppress"]
-texts = df[1]
 
-indexes_of_interest_per_pat = []
-for p in props:
-    indexes_of_interest_per_pat += [
-        [match_pattern(p, x, suffix_length=0) for x in texts]
+props_path = "data/test/uni_props.txt"
+with open(props_path, "r") as f:
+    all_props = f.read().split("\n")
+
+
+
+def extract_and_embed_mentions(props, texts):
+
+    indexes_of_interest_per_pat = []
+    for p in props:
+        indexes_of_interest_per_pat += [
+            [match_pattern(p, x, suffix_length=0) for x in texts]
+        ]
+
+    flags = [
+        any(True if ixs_ else False for ixs_ in item)
+        for item in zip(*indexes_of_interest_per_pat)
     ]
 
-flags = [
-    any(True if ixs_ else False for ixs_ in item)
-    for item in zip(*indexes_of_interest_per_pat)
-]
+    data = [t for flag, t in zip(flags, texts) if flag]
 
-data = [t for flag, t in zip(flags, texts) if flag]
+    batch_size = 40
 
-batch_size = 40
+    data_batched = [data[i : i + batch_size] for i in range(0, len(data), batch_size)]
 
-data_batched = [data[i : i + batch_size] for i in range(0, len(data), batch_size)]
-
-frep = []
-tt_averages = []
+    frep = []
+    tt_averages = []
 
 
-for batch in (pbar := tqdm.tqdm(data_batched)):
-    report = texts_to_vrep(
-        batch,
-        tokenizer,
-        M,
-        [1, 2],
-        word_modes=[WordGrouping.W1],
-    )
+    for batch in (pbar := tqdm.tqdm(data_batched)):
+        report = texts_to_vrep(
+            batch,
+            tokenizer,
+            M,
+            [1, 2],
+            word_modes=[WordGrouping.W1],
+        )
 
-    for p in props:
-        normalized_texts = report["normalized_text"]
-        word_groupings = report["word_groupings"]
+        for p in props:
+            normalized_texts = report["normalized_text"]
+            word_groupings = report["word_groupings"]
 
-        for w, r_item in word_groupings.items():
-            for jsent, (text, report_sent) in enumerate(zip(normalized_texts, r_item)):
-                if p == props[0] and w == sorted(word_groupings)[0]:
-                    tt0 = [t for _, t in report_sent]
-                    tt_averages += tt0
+            for w, r_item in word_groupings.items():
+                for jsent, (text, report_sent) in enumerate(zip(normalized_texts, r_item)):
+                    if p == props[0] and w == sorted(word_groupings)[0]:
+                        tt0 = [t for _, t in report_sent]
+                        tt_averages += tt0
 
-                indexes_of_interest_batched = match_pattern(p, text, suffix_length=0)
+                    indexes_of_interest_batched = match_pattern(p, text, suffix_length=0)
 
-                if not indexes_of_interest_batched:
-                    continue
+                    if not indexes_of_interest_batched:
+                        continue
 
-                report_sent = sorted(report_sent, key=lambda x: x[0]["a"])
+                    report_sent = sorted(report_sent, key=lambda x: x[0]["a"])
 
-                _, map_ij = map_spans_to_spans(
-                    [(x["a"], x["b"]) for x, _ in report_sent],
-                    indexes_of_interest_batched,
-                )
+                    _, map_ij = map_spans_to_spans(
+                        [(x["a"], x["b"]) for x, _ in report_sent],
+                        indexes_of_interest_batched,
+                    )
 
-                tts = [
-                    torch.stack([t for _, t in report_sent[ja:jb]]).mean(0)
-                    for ja, jb in map_ij
-                ]
+                    tts = [
+                        torch.stack([t for _, t in report_sent[ja:jb]]).mean(0)
+                        for ja, jb in map_ij
+                    ]
 
-                mentions = [
-                    " ".join([x["mention"] for x, _ in report_sent[ja:jb]])
-                    for (ja, jb) in map_ij
-                ]
+                    mentions = [
+                        " ".join([x["mention"] for x, _ in report_sent[ja:jb]])
+                        for (ja, jb) in map_ij
+                    ]
 
-                frep += [(w, jsent, p, m, tt) for m, tt in zip(mentions, tts)]
+                    frep += [(w, jsent, p, m, tt) for m, tt in zip(mentions, tts)]
 
-    pbar.set_description(f"entities added : {len(frep)}")
+        pbar.set_description(f"entities added : {len(frep)}")
 
-tt_all = (torch.stack([x[4] for x in frep]), "all.patterns")
-tt_pats = [(torch.stack([x[4] for x in frep if x[2] == p]), p) for p in props]
+    uprops = np.unique([x[2] for x in frep])
+    tt_pats = {p:torch.stack([x[4] for x in frep if x[2] == p]) for p in uprops}
+
+    return tt_pats
