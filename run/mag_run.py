49f64ead22eefaeaaf56c46db5f5196097f3f7be
Alexander Belikov <abelikov@gmail.com>
2025-07-29 21:38:12 +0200

    added clustering

--- run/mag_run.py
+++ run/mag_run.py
@@ -1,10 +1,7 @@
 import spacy
 import torch
-import pickle
-import numpy as np
 import pandas as pd
 import tqdm
-import pdb
 
 from pelinker.matching import match_pattern
 from pelinker.util import texts_to_vrep
@@ -22,12 +19,7 @@ tokenizer = AutoTokenizer.from_pretrained("NeuML/pubmedbert-base-embeddings")
 nlp = spacy.load("en_core_web_sm")
 
 data_path = "data/jamshid/bio_mag_2M.tsv.gz"
-df = pd.read_csv(data_path, 
-                 sep="\t", 
-                 header=None, 
-                 compression='gzip',
-                 chunksize=2000
-        )
+df = pd.read_csv(data_path, sep="\t", header=None, compression="gzip", chunksize=2000)
 
 
 props_path = "data/test/uni_props.txt"
@@ -36,23 +28,20 @@ with open(props_path, "r") as f:
 
 
 def run_on_selected_texts():
-    
     res = []
     save_path = "data/jamshid/bio_2M_res.feather"
-    for i,chunk in tqdm.tqdm(enumerate(df), leave=True, position=0):
-
+    for i, chunk in tqdm.tqdm(enumerate(df), leave=True, position=0):
         chunk_texts = list(chunk[1])
         pmids = list(chunk[0])
         res = extract_and_embed_mentions(all_props, chunk_texts, pmids, res)
-        with open("data/jamshid/log", 'w') as f:
+        with open("data/jamshid/log", "w") as f:
             f.write(str(i))
 
-        if not(i%20):
+        if not (i % 20):
             res.to_feather(save_path)
 
 
 def extract_and_embed_mentions(props, texts, pmids, embeds_df=[]):
-
     indexes_of_interest_per_pat = []
     for p in props:
         indexes_of_interest_per_pat += [
@@ -70,17 +59,16 @@ def extract_and_embed_mentions(props, texts, pmids, embeds_df=[]):
     batch_size = 40
 
     data_batched = [data[i : i + batch_size] for i in range(0, len(data), batch_size)]
-    data_pmids_batched = [data_pmids[i : i + batch_size] for i in range(0, len(data), batch_size)]
+    data_pmids_batched = [
+        data_pmids[i : i + batch_size] for i in range(0, len(data), batch_size)
+    ]
 
-    if len(embeds_df)==0:
-        embeds_df = pd.DataFrame(
-            [], 
-            columns=['pmid', 'property', 'mention', 'embed']
-        )
-        
+    if len(embeds_df) == 0:
+        embeds_df = pd.DataFrame([], columns=["pmid", "property", "mention", "embed"])
+
     tt_averages = []
 
-    for i,batch in (pbar := tqdm.tqdm(enumerate(data_batched))):
+    for i, batch in (pbar := tqdm.tqdm(enumerate(data_batched))):
         report = texts_to_vrep(
             batch,
             tokenizer,
@@ -88,7 +76,7 @@ def extract_and_embed_mentions(props, texts, pmids, embeds_df=[]):
             [1, 2],
             word_modes=[WordGrouping.W1],
         )
-        # next line assumes that "texts_to_vrep" function returns exactly the same input text as 
+        # next line assumes that "texts_to_vrep" function returns exactly the same input text as
         # the "normalized_text" of the output dictionary, hence using their PMIDs as is
         batch_pmids = data_pmids_batched[i]
 
@@ -97,12 +85,16 @@ def extract_and_embed_mentions(props, texts, pmids, embeds_df=[]):
             word_groupings = report["word_groupings"]
 
             for w, r_item in word_groupings.items():
-                for jsent, (text, report_sent) in enumerate(zip(normalized_texts, r_item)):
+                for jsent, (text, report_sent) in enumerate(
+                    zip(normalized_texts, r_item)
+                ):
                     if p == props[0] and w == sorted(word_groupings)[0]:
                         tt0 = [t for _, t in report_sent]
                         tt_averages += tt0
 
-                    indexes_of_interest_batched = match_pattern(p, text, suffix_length=0)
+                    indexes_of_interest_batched = match_pattern(
+                        p, text, suffix_length=0
+                    )
 
                     if not indexes_of_interest_batched:
                         continue
@@ -123,22 +115,22 @@ def extract_and_embed_mentions(props, texts, pmids, embeds_df=[]):
                         " ".join([x["mention"] for x, _ in report_sent[ja:jb]])
                         for (ja, jb) in map_ij
                     ]
-                    
-                    for m,tt in zip(mentions, tts):
-                        
+
+                    for m, tt in zip(mentions, tts):
                         tmp = pd.DataFrame(
-                            {'pmid': batch_pmids[jsent], 'property': p, 'mention': m, 'embed':[tt.numpy()]}, 
-                            index=[0]
+                            {
+                                "pmid": batch_pmids[jsent],
+                                "property": p,
+                                "mention": m,
+                                "embed": [tt.numpy()],
+                            },
+                            index=[0],
                         )
 
                         embeds_df = pd.concat(
                             [embeds_df, tmp], axis=0, ignore_index=True
-                        ) 
+                        )
 
         pbar.set_description(f"entities added : {len(embeds_df)}")
 
     return embeds_df
-
-
-
-
